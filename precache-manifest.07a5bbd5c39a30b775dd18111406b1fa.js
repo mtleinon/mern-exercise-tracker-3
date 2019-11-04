0332611b9301c1a3bf0f5a73ab7c999b43bdaad0
mtleinon <mika.t.leinonen@gmail.com>
2019-11-04 10:12:15 +0200

    Updates

---
+++ precache-manifest.07a5bbd5c39a30b775dd18111406b1fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e35c70da2fe428dcd09a395d31b18152",
+    "url": "/mern-exercise-tracker-2/index.html"
+  },
+  {
+    "revision": "a4a4fbc7e60fe44a2c95",
+    "url": "/mern-exercise-tracker-2/static/css/2.7d576c58.chunk.css"
+  },
+  {
+    "revision": "a4a4fbc7e60fe44a2c95",
+    "url": "/mern-exercise-tracker-2/static/js/2.ab32f552.chunk.js"
+  },
+  {
+    "revision": "84dfd48e1b618d99fdfb",
+    "url": "/mern-exercise-tracker-2/static/js/main.195ac038.chunk.js"
+  },
+  {
+    "revision": "6df454e8a5d7fa3e3702",
+    "url": "/mern-exercise-tracker-2/static/js/runtime-main.9c3be62c.js"
+  }
+]);
